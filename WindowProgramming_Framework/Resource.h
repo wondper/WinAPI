b703fc4c552d7f0055019227c6ff38b73e39148a
DoomDoom5 <44747048+DoomDoom5@users.noreply.github.com>
2022-05-29 04:07:42 +0900

    백그라운드 비트맵 추가, 트로이 목마 비트맵 추가, 아이템과 목마 파일 이름 변경

--- WindowProgramming_Framework/Resource.h
+++ WindowProgramming_Framework/Resource.h
@@ -13,12 +13,27 @@
 #define IDC_WINDOWPROGRAMMINGFRAMEWORK  109
 #define IDR_MAINFRAME                   128
 #define IDB_BITMAP1                     134
-#define IDB_MONSTER_HORSE1_MASK         136
+#define IDB_MONSTER_HORSE               136
+#define IDB_MONSTER_BOSS1               136
 #define IDB_BG_STAGE1                   137
 #define IDB_BG_STAGE2                   138
 #define IDB_BG_STAGE3                   139
 #define IDB_ITEM1                       140
 #define IDB_ITEM2                       141
+#define IDB_MONSTER_HORSE1              142
+#define IDB_ITEM_BULLET                 142
+#define IDB_ITEM_CAKE                   143
+#define IDB_MONSTER_HORSE3              144
+#define IDB_MONSTER_HORSE4              145
+#define IDB_MONSTER_BOSS2               145
+#define IDB_MONSTER_HORSE2              146
+#define IDB_MONSTER_BOSS3               146
+#define IDB_MONSTER_HORSE5              147
+#define IDB_MONSTER_BOSS4               147
+#define IDB_MONSTER_BOSS5               148
+#define IDB_MONSTER_BOSS16              149
+#define IDB_MONSTER_BOSS6               149
+#define IDB_BG_STAGE4                   150
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -26,7 +41,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        141
+#define _APS_NEXT_RESOURCE_VALUE        150
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
