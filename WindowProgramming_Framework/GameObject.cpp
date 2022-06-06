a0d87fb8373d139ca08b366aca10118f97d27e7d
DoomDoom5 <44747048+DoomDoom5@users.noreply.github.com>
2022-06-06 17:33:02 +0900

    GameObject 구조 설정, 상속받은 객체들 순수가상함수 설정해야함. BackGroundWnd 에 Bitmap 그리기 성공

--- WindowProgramming_Framework/GameObject.cpp
+++ WindowProgramming_Framework/GameObject.cpp
@@ -1,8 +1,14 @@
 #include "GameObject.h"
 
-GameObject::GameObject()
-	: mHP{ 1 }, mPosition{0, 0}
+GameObject::GameObject(int ResCode)
+	: mHP{ 1 }, mPosition{ 0, 0 }, mWidth{ 0 }, mHeight{ 0 }
 {
+	BITMAP bmp;
+	for (int i = 0; i < 6; ++i)
+		mAppearanceBitmap[i] = (HBITMAP)LoadBitmap(g_hInst, MAKEINTRESOURCE(ResCode));
+	GetObject(mAppearanceBitmap[0], sizeof(BITMAP), &bmp);
+	SetBitmapFrame(bmp.bmWidth, bmp.bmHeight);
+	SetType(0);
 
 }
 
@@ -10,3 +16,57 @@ void GameObject::PlaySound()
 {
 	Beep(1, 10);
 }
+
+void GameObject::DrawBitmap(HDC hdc, HDC memdc)
+{
+	HBITMAP oldBit;
+	oldBit = (HBITMAP)SelectObject(memdc, mAppearanceBitmap[0]);
+	BitBlt(hdc, mPosition.x, mPosition.y, mWidth, mHeight, memdc, 0, 0, SRCCOPY);
+	SelectObject(memdc, oldBit);
+	DeleteDC(memdc);
+}
+
+Cake::Cake() : GameObject(IDB_ITEM_CAKE)
+{
+
+}
+
+Cake::~Cake()
+{
+
+}
+
+void Cake::PlaySound()
+{
+
+}
+
+void Cake::Anim(char Action)
+{
+
+}
+
+void Cake::DrawBitmap(HDC hdc, HDC memdc)
+{
+	GameObject::DrawBitmap(hdc, memdc);
+}
+//
+//Megazine::~Megazine()
+//{
+//
+//}
+//
+//Zombie::~Zombie()
+//{
+//
+//}
+//
+//Bat::~Bat()
+//{
+//
+//}
+//
+//Boss::~Boss()
+//{
+//
+//}
--- WindowProgramming_Framework/GameObject.h
+++ WindowProgramming_Framework/GameObject.h
@@ -1,17 +1,84 @@
 #pragma once
 #include "stdafx.h"
+#include "resource.h"
+
+enum class OBJECT_TYPE {CAKE, MAGAZINE, ZOMBIE, BAT, BOSS};
+
+extern HINSTANCE g_hInst;
 
 class GameObject
 {
 private:
+	char mType = -1;
 	int mHP;
+
+	int mWidth;
+	int mHeight;
+
 	POINT mPosition;
-	HBITMAP mAppearanceBitmap;
+	HBITMAP mAppearanceBitmap[6];
 public:
-	GameObject();
-	virtual ~GameObject() = 0;
+	GameObject(int ResCode);
+	virtual ~GameObject(){};
 
 	virtual void PlaySound();
-	virtual void Anim(char Type) = 0; // Type에 따라 다른 애니메이션 설정.
+	virtual void Anim(char Action) = 0; // Type에 따라 다른 애니메이션 설정.
+	virtual void DrawBitmap(HDC hdc, HDC memdc);
+
+
+	int GetHP() const { return mHP; }
+	void SetHP(int hp) { mHP = hp; }
+
+	void SetType(char type) { mType = type; }
+	
+	void SetBitmapFrame(int width, int height) { mWidth = width; mHeight = height; }
+
+	void SetPosition(const POINT& Pos) { mPosition = Pos; }
+
+
 };
 
+class Cake : public GameObject
+{
+private:
+
+public:
+	Cake();
+	virtual ~Cake();
+	virtual void PlaySound();
+	virtual void Anim(char Action);
+	virtual void DrawBitmap(HDC hdc, HDC memdc);
+
+};
+//
+//class Megazine : public GameObject
+//{
+//private:
+//public:
+//	Megazine();
+//	virtual ~Megazine();
+//};
+//
+//class Zombie : public GameObject
+//{
+//private:
+//public:
+//	Zombie();
+//	virtual	~Zombie();
+//};
+//
+//class Bat : public GameObject
+//{
+//private:
+//public:
+//	Bat();
+//	virtual ~Bat();
+//};
+//
+//class Boss : public GameObject
+//{
+//private:
+//public:
+//	Boss();
+//	virtual ~Boss();
+//};
